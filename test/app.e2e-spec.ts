316cf163ee72fa6d88260c7746322d3ebb05fd5a
Andrei Vasile <andrei.vasile97@gmail.com>
2022-01-27 00:44:35 +0200

    Add e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('PokemonController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +15,42 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/pokemon/:pokemonName (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/pokemon/mewtwo')
       .expect(200)
-      .expect('Hello World!');
+      .expect({
+        name: 'mewtwo',
+        description:
+          'It was created by\na scientist after\nyears of horrific\fgene splicing and\nDNA engineering\nexperiments.',
+        habitat: 'rare',
+        isLegendary: true,
+      });
+  });
+
+  it('/pokemon/translated/:pokemonName (GET) - yoda translation', () => {
+    return request(app.getHttpServer())
+      .get('/pokemon/translated/mewtwo')
+      .expect(200)
+      .expect({
+        name: 'mewtwo',
+        description:
+          'Created by a scientist after years of horrific gene splicing and dna engineering experiments,  it was.',
+        habitat: 'rare',
+        isLegendary: true,
+      });
+  });
+
+  it('/pokemon/translated/:pokemonName (GET) - shakespeare translation', () => {
+    return request(app.getHttpServer())
+      .get('/pokemon/translated/tentacool')
+      .expect(200)
+      .expect({
+        name: 'tentacool',
+        description:
+          "Drifts in shallow flotes. Anglers who is't hook those folk by accident art oft did amerce by its stinging acid.",
+        habitat: 'sea',
+        isLegendary: false,
+      });
   });
 });
